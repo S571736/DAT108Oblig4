a33b107527f1d5dfd2b3625f10c853a2d0bb6f94
S571736 <Sondre.Gjesdal@gmail.com>
2018-11-14 11:04:50 +0100

    ikkje slette når man legger til

--- src/no/hvl/dat108/Graf.java
+++ src/no/hvl/dat108/Graf.java
@@ -11,14 +11,14 @@ public class Graf {
 
 
     //Slett funker betre.
-    public Kant fjernKant(int kantNr){
+    public Kant fjernKant(int kantNr) {
         kant = kanter.get(kantNr);
         //Sletter ein node som ender opp uten kanter
         //Sette opp ja nei om å slette?
-        if(kant.tilkobletNode.size() != 0){
-            for (int i = 0; i < kant.tilkobletNode.size(); i++){
+        if (kant.tilkobletNode.size() != 0) {
+            for (int i = 0; i < kant.tilkobletNode.size(); i++) {
                 node = kant.tilkobletNode.get(i);
-                if (node.tilkobletKant.size() == 0){
+                if (node.tilkobletKant.size() == 0) {
                     kant.tilkobletNode.remove(i);
                 }
             }
@@ -27,9 +27,9 @@ public class Graf {
         return kant;
     }
 
-    public Node fjernNode(int nodeNr){
+    public Node fjernNode(int nodeNr) {
         node = noder.get(nodeNr);
-        if (node.tilkobletKant.size() != 0){
+        if (node.tilkobletKant.size() != 0) {
             for (int i = 0; i < node.tilkobletKant.size(); i++) {
                 node.tilkobletKant.remove(i);
             }
@@ -38,33 +38,34 @@ public class Graf {
         return node;
     }
 
-    public void slettKant(Kant sletteKant, Node sletteNode){
-        for(int i = 0; i < sletteNode.tilkobletKant.size(); i++){
-            if (sletteNode.tilkobletKant.get(i) == sletteKant){
+    public void slettKant(Kant sletteKant, Node sletteNode) {
+        for (int i = 0; i < sletteNode.tilkobletKant.size(); i++) {
+            if (sletteNode.tilkobletKant.get(i) == sletteKant) {
                 sletteNode.tilkobletKant.remove(i);
             }
         }
     }
 
-    public ArrayList<Node> breddeFørst(){
+    public ArrayList<Node> breddeFørst() {
         ArrayList<Node> ko = new ArrayList<>();
         ArrayList<Node> bredde = new ArrayList<>();
         Node ekstraNode;
         ko.add(noder.get(0));
 
-        while(ko.size()!= 0){
+        while (ko.size() != 0) {
             node = ko.get(0);
             System.out.println(node.toString());
-            for (int i = 0; i < node.tilkobletKant.size(); i++){
+            for (int i = 0; i < node.tilkobletKant.size(); i++) {
                 kant = node.tilkobletKant.get(i);
-                //sletter kanten fra tilkobla node
-                if (kant.tilkobletNode.get(0) != node){
+
+                if (kant.tilkobletNode.get(0) != node) {
                     ekstraNode = kant.tilkobletNode.get(0);
-                    slettKant(kant, ekstraNode);
-                }else{
+                    ko.add(ekstraNode);
+                } else {
                     ekstraNode = kant.tilkobletNode.get(1);
-                    slettKant(kant, ekstraNode);
+                    ko.add(ekstraNode);
                 }
+
 
             }
         }
